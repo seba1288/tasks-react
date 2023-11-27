e83d0ea3de0c7bf522a19236fd8fd0490caea1db
Sebastian Wróbel <80533347+seba1288@users.noreply.github.com>
2023-11-27 11:49:15 +0100

    Initial commit

---
+++ src/components/TitleText/TitleText.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { title } from './TitleText.module.scss';
+
+export const TitleText = () => (
+  <h1 className={`${title} bg-dark`}>Hello on team-homepage</h1>
+);
